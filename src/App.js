7ad2ba842b6f8d74b89b23809e467516d3d8031f
FedkinAndrey <fedkin17@gmail.com>
2021-03-04 19:48:19 +0200

    sorting filtering use prop-types and React.memo for SortPopup.jsx and Categories.jsx actions and reducers was modified added react-thunk and react-content-loader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import axios from "axios";
-import {useDispatch} from 'react-redux'
 
 import {Header} from './components';
-import {Home, Cart} from "./pages";
+import {Cart, Home} from "./pages";
 import {Route} from "react-router-dom";
-import {setPizzas} from './redux/actions/pizzas'
 
 function App() {
-  const dispatch = useDispatch()
-
-  React.useEffect(() => {
-    // move to Redux and use redux-thunk
-    // track filters and sorting and use params in URL from Redux
-    // add imitation of loading of pizzas (CSS and PizzaBlock)
-    axios.get('http://localhost:3001/pizzas').then(({data}) => {
-      dispatch(setPizzas(data))
-    })
-  }, [])
-
   return (
     <div className="wrapper">
       <Header/>
@@ -30,31 +16,5 @@ function App() {
   )
 }
 
-/*class App extends React.Component {
-  componentDidMount() {
-    axios.get('http://localhost:3000/db.json').then(({data}) => {
-      this.props.setPizzas(data.pizzas)
-    })
-  }
-
-  render() {
-    // console.log(this.props)
-    return (
-      <div className="wrapper">
-        <Header/>
-        <div className="content">
-          <Route path="/" render={() => <Home items={this.props.items}/>} exact/>
-          <Route path="/cart" component={Cart} exact/>
-        </div>
-      </div>
-    )
-  }
-}*/
-
-
 export default App
-//
-// return {
-//   items: state.pizzas.items,
-//   filters: state.filters
-// }
+
